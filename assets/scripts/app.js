aa46d43369d53da90c45938a5cdf5194a20b4baa
dotrong <dohieu@gmail.com>
2017-05-05 23:42:21 -0700

    still progress

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -18,17 +18,23 @@ var dbConnectedRef = database.ref('.info/connected');
 
 var dbPlayerRef  = database.ref('/players');
 
-var numPlayer = 0;
+var dbChatRef  = database.ref('/chat');
 
 var con;
+
+var userId;
+
+var userNode;
+
+var userName;
 
 dbPlayerRef.on("child_added", function(snapshot) {
 
 	var userObject = snapshot.val();
 
-	console.log(userObject);
+	//if (($('#player1').html().trim() == "") && ($('#player2').html().trim() == "")) {
 
-	if ($('#player1').html().trim() == "") {
+	if (userObject.userId == userId) {
 
 		var p = $('<p>');
 		p.text('Player: '+ userObject.userName);
@@ -45,27 +51,19 @@ dbPlayerRef.on("child_added", function(snapshot) {
 		scissorsButton.text('Scissors');
 		scissorsButton.attr('id','scissors');
 
+		$('#player1').attr("userId",userObject.userId);
+
 		$('#player1').append(p,rockButton,'<br>',paperButton,'<br>',scissorsButton);
 
 	}
-	else if ($('#player2').html().trim() == ""){
+	else {
 
 		var p = $('<p>');
 		p.text('Player: '+ userObject.userName);
 
-		var rockButton = $('<button>');
-		rockButton.text('Rock');
-		rockButton.attr('id','rock2');
+		$('#player2').attr("userId",userObject.userId);
 
-		var paperButton = $('<button>');
-		paperButton.text('Paper');
-		paperButton.attr('id','paper2');
-
-		var scissorsButton = $('<button>');
-		scissorsButton.text('Scissors');
-		scissorsButton.attr('id','scissors2');
-
-		$('#player2').append(p,rockButton,'<br>',paperButton,'<br>',scissorsButton);
+		$('#player2').append(p);
 
 	}
 
@@ -73,7 +71,7 @@ dbPlayerRef.on("child_added", function(snapshot) {
 
 $("#start").on("click", function() {
 
-	var userName = $("#userName").val().trim();
+	userName = $("#userName").val().trim();
 
 	$("#userName").val('');
 
@@ -81,25 +79,185 @@ $("#start").on("click", function() {
 
 		if (snapshot.val()) {
 
+			userId = dbPlayerRef.push().key;
+
 			con = dbPlayerRef.push({
 
 				userName: userName,
 				win:0,
 				loss: 0,
-				state:'ready'
+				state:'ready',
+				userId: userId
 
 			});
 
-			//console.log(dbPlayerRef.push().key());
+			userNode = con.key;
 
 			// Remove user from the connection list when they disconnect.
 			con.onDisconnect().remove();
-
+		
 		}
 
 	});
 
 });
 
+$("#player1").on("click","button",function() {
+
+	if ($(this).attr('id') == 'rock') {
+
+		var dbRef = database.ref('/players' + '/'+userNode);
+
+		dbRef.update({ state: 'rock'});
+	}
+
+	else if ($(this).attr('id') == 'paper') {
+
+		var dbRef = database.ref('/players' + '/'+userNode);
+
+		dbRef.update({ state: 'paper'});
+	}
+
+	else if ($(this).attr('id') == 'scissors') {
+
+		var dbRef = database.ref('/players' + '/'+userNode);
+
+		dbRef.update({ state: 'scissors'});
+	}
 
 
+});
+
+
+dbPlayerRef.on("child_removed", function(snapshot) {
+
+	console.log(snapshot.val());
+
+	var userRemoveObj = snapshot.val();
+
+	$('div[userId='+ userRemoveObj.userId +']').html('');
+	$('div[userId='+ userRemoveObj.userId +']').removeAttr('userId');
+
+
+});
+
+var selection = [];
+
+
+dbPlayerRef.on("child_changed", function(snapshot) {
+
+	var userChanged = snapshot.val();
+
+	selection.push({
+
+		userId: userChanged.userId,
+		state: userChanged.state
+
+	});
+
+	console.log(selection);
+
+	if (selection.length == 2) {
+
+		console.log("enough");
+
+			var playerId_0 = selection[0].userId;
+			var playerSelect_0 = selection[0].state;
+
+			var playerId_1 = selection[1].userId;
+			var playerSelect_1 = selection[1].state;
+
+			var result = gameResult(playerSelect_0,playerSelect_1);
+
+			if (result == 1) {
+
+				//playerId_0 is winner
+
+				$('div[userId='+ playerId_0 +']').append('<p> You are the winner </p>');
+
+				$('div[userId='+ playerId_1 +']').append('<p> You lose! </p>');
+
+
+			}
+
+			else if (result == 2) {
+				//playerId_1 is winner
+
+				$('div[userId='+ playerId_1 +']').append('<p> You are the winner </p>');
+
+				$('div[userId='+ playerId_0 +']').append('<p> You lose! </p>');
+			}
+			else {
+				//draw
+
+				$('div[userId='+ playerId_1 +']').append('<p> Draw </p>');
+
+				$('div[userId='+ playerId_0 +']').append('<p> Draw </p>');
+			}
+
+
+
+
+	}
+
+});
+
+
+
+function gameResult(player1, player2) {
+
+	if (((player1 == 'rock') && (player2 == 'scissors')) || ((player1 == 'paper') && (player2 == 'rock')) || 
+		((player1 == 'scissors') && (player2 == 'paper'))) {
+		return 1;
+	}
+
+	else if (((player2 == 'rock') && (player1 == 'scissors')) || ((player2 == 'paper') && (player1 == 'rock')) || 
+		((player2 == 'scissors') && (player1 == 'paper'))) {
+		return 2;
+	}
+	else {
+		return 3;
+	}
+
+
+}
+
+$("#send").on("click", function() {
+
+	var msg = $('#chatMsg').val().trim();
+
+	$('#chatMsg').val('');
+
+	if (msg != '') {
+
+		var con = dbChatRef.push({
+
+			message: userName + ': ' + msg,
+
+		});
+
+		con.onDisconnect().remove();
+
+	}
+
+
+
+});
+
+dbChatRef.on("child_added", function(snapshot) {
+
+	var message = snapshot.val().message;
+
+	$("#chatArea").append('<p>' + message + '</p>');
+
+
+});
+
+dbChatRef.on("child_removed", function(snapshot) {
+
+	dbChatRef.remove();
+
+	$("#chatArea").html('');
+
+
+});
